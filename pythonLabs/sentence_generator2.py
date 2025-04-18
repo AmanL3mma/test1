446f83f22ac71001da99788163299bddcf8bb395
lemma.aman@yahoo.com <lemma.aman@yahoo.com>
2025-04-18 11:33:49 -0400

    sentence_generator2.py

--- pythonLabs/sentence_generator2.py
+++ pythonLabs/sentence_generator2.py
@@ -1,10 +1,10 @@
 import random
 
-s_nouns = ["A dude", "My mom", "The king", "Some guy", "A cat with rabies", "A sloth", "Your homie", "This cool guy my gardener met yesterday", "Superman"]
-p_nouns = ["These dudes", "Both of my moms", "All the kings of the world", "Some guys", "All of a cattery's cats", "The multitude of sloths living under your bed", "Your homies", "Like, these, like, all these people", "Supermen"]
-s_verbs = ["eats", "kicks", "gives", "treats", "meets with", "creates", "hacks", "configures", "spies on", "retards", "meows on", "flees from", "tries to automate", "explodes"]
-p_verbs = ["eat", "kick", "give", "treat", "meet with", "create", "hack", "configure", "spy on", "retard", "meow on", "flee from", "try to automate", "explode"]
-infinitives = ["to make a pie.", "for no apparent reason.", "because the sky is green.", "for a disease.", "to be able to make toast explode.", "to know more about archeology."]
+s_nouns = ["A lion", "My friend", "The Emperor", "Some guy", "A cat with rabies", "A bear", "Your homie", "This cool guy my gardener met yesterday", "Namor"]
+p_nouns = ["These dudes", "Both of my friends", "All the Emperors of the world", "Some guys", "All of a cattery's cats", "The multitude of bears living under your bed", "Your homies", "Like, these, like, all these people", "Atlanteans"]
+s_verbs = ["eats", "punches", "gives", "treats", "meets with", "creates", "attacks", "configures", "spies on", "retards", "roars on", "flees from", "tries to automate", "explodes"]
+p_verbs = ["eat", "punch", "give", "treat", "meet with", "create", "attack", "configure", "spy on", "retard", "roar on", "flee from", "try to automate", "explode"]
+infinitives = ["to make a pie.", "for no apparent reason.", "because the ocean is poluted.", "for a disease.", "to avenge the kingdom.", "to know more about the surface world."]
 
 def sing_sen_maker():
     '''Makes a random senctence from the different parts of speech. Uses a SINGULAR subject'''
