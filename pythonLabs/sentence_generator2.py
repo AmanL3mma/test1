c51493859d8df4f257630a5762c63b82f2a6caaa
lemma.aman@yahoo.com <lemma.aman@yahoo.com>
2025-05-03 13:27:26 -0400

    sentencegenerator

--- pythonLabs/sentence_generator2.py
+++ pythonLabs/sentence_generator2.py
@@ -1,10 +1,10 @@
 import random
 
-s_nouns = ["A lion", "My friend", "The Emperor", "Some guy", "A cat with rabies", "A bear", "Your homie", "This cool guy my gardener met yesterday", "Namor"]
-p_nouns = ["These dudes", "Both of my friends", "All the Emperors of the world", "Some guys", "All of a cattery's cats", "The multitude of bears living under your bed", "Your homies", "Like, these, like, all these people", "Atlanteans"]
+s_nouns = ["A lion", "My friend", "The Emperor", "Some guy", "A squid with anger issues", "A bear", "Your homie", "This cool guy my gardener met yesterday", "Namor"]
+p_nouns = ["These dudes", "Both of my friends", "All the Emperors of the world", "Some guys", "All of a aquariums squids", "The multitude of bears living under your bed", "Your homies", "Like, these, like, all these people", "Atlanteans"]
 s_verbs = ["eats", "punches", "gives", "treats", "meets with", "creates", "attacks", "configures", "spies on", "retards", "roars on", "flees from", "tries to automate", "explodes"]
 p_verbs = ["eat", "punch", "give", "treat", "meet with", "create", "attack", "configure", "spy on", "retard", "roar on", "flee from", "try to automate", "explode"]
-infinitives = ["to make a pie.", "for no apparent reason.", "because the ocean is poluted.", "for a disease.", "to avenge the kingdom.", "to know more about the surface world."]
+infinitives = ["to make clam chowder.", "for no apparent reason.", "because the ocean is poluted.", "for a disease.", "to avenge the kingdom.", "to know more about the surface world."]
 
 def sing_sen_maker():
     '''Makes a random senctence from the different parts of speech. Uses a SINGULAR subject'''
