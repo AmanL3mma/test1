1a12dee24dd87855544e41392edf40d67d3d186a
lemma.aman@yahoo.com <lemma.aman@yahoo.com>
2025-07-16 16:45:41 -0400

    scouter.py

---
+++ pythonLabs/scouter.py
@@ -0,0 +1,9 @@
+PowerLevel = int(input("Vegeta! What does the scouter say about his power level?!"))
+if PowerLevel >= 9001:
+    print("It's over 9000!!!")
+elif PowerLevel > 5000:
+    print("He's strong, Nappa. Stay alert.")
+elif PowerLevel > 3500:
+    print("Just an Earthling, whoop his ass.") 
+elif PowerLevel ==5:
+    print("Your power level is 5? Scum...")
